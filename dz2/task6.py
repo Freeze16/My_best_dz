98188ea97ffb9d3792fbb5300077047b9b4dfdcc
Freeze16 <gnetov.i@mail.ru>
2023-09-30 11:46:36 +0300

    dz2_v1.2

--- dz2/task6.py
+++ dz2/task6.py
@@ -1,4 +1,13 @@
+# a = input()
+# numbers = [i for i in a if i in map(str, range(10))]
+# k = int(input())
+# print(f'{k}-ая цифра в строке {numbers[k - 1]}')
+
 a = input()
-numbers = [i for i in a if i in map(str, range(10))]
-k = int(input())
-print(f'{k}-ая цифра в строке {numbers[k - 1]}')
+k = k0 = int(input())
+
+for i in a:
+    if i in map(str, range(10)):
+        k -= 1
+    if k == 0:
+        print(f'{k0}-ая цифра в строке {i}')
