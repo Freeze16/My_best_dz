1aa8e9e231e7ebedaa7a5080d5854fe386ff8ab6
Freeze16 <gnetov.i@mail.ru>
2023-10-13 22:25:46 +0300

    dz2_edit

--- dz2/task2.py
+++ dz2/task2.py
@@ -1,1 +1,11 @@
-# Придумаю что-нибудь позже
+from dz3.task1 import enter
+
+
+def max_number(a):
+    b = len(max(a, key=len))
+    a.sort(key=lambda x: x * b, reverse=True)
+    return int(''.join(a))
+
+
+max_number = max_number(enter(input()))
+print(max_number)
