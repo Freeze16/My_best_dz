1eb39726b0cc4c31f89fc4fcad331d1d3809a934
Freeze16 <gnetov.i@mail.ru>
2023-10-14 10:29:20 +0300

    micro_fix

--- dz3/task6.py
+++ dz3/task6.py
@@ -1,11 +1,11 @@
-def check(number, current_number=2):
+def check(number: int, current_number=2):
     if number % current_number == 0:
         return current_number
 
     return check(number, current_number + 1)
 
 
-def check_simple(n):
+def check_simple(n: int) -> bool:
     if n == 1:
         return False
     return bool(check(n) == n)
