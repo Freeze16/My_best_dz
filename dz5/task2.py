e5fad38909578e7ef28b2cc540e22616f6f033a1
Freeze16 <gnetov.i@mail.ru>
2023-11-11 10:20:30 +0300

    test_version

--- dz5/task2.py
+++ dz5/task2.py
@@ -1,7 +1,8 @@
 from dz3.task1 import enter
+from typing import Union
 
 
-def bin_search(pos: list[float], el: float) -> int:
+def bin_search(pos: list[Union[int, float]], el: Union[int, float]) -> int:
     index, min_index, max_index = len(pos) // 2, 0, len(pos) - 1
     while pos[index] != el and min_index <= max_index:
         if pos[index] < el:
